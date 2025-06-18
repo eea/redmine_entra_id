afb266d47c95e2d59c5db9fc1d907a060a767ef1
Andrei Maxim <andrei@andreimaxim.ro>
2025-06-19 00:24:13 +0300

    Add filtering and sorting by sync time

--- lib/entra_id/user_query_patch.rb
+++ lib/entra_id/user_query_patch.rb
@@ -3,15 +3,16 @@
 module EntraId::UserQueryPatch
   def self.included(base)
     base.class_eval do
-      # Add OID column to available columns
       self.available_columns << QueryColumn.new(:oid, sortable: "#{User.table_name}.oid", caption: :field_entra_id_oid)
+      self.available_columns << QueryColumn.new(:synced_at, sortable: "#{User.table_name}.synced_at", caption: :field_entra_id_synced_at)
 
-      # Override initialize_available_filters to add OID filter
       alias_method :initialize_available_filters_without_oid, :initialize_available_filters
 
       def initialize_available_filters
         initialize_available_filters_without_oid
+
         add_available_filter "oid", type: :string, label: :field_entra_id_oid
+        add_available_filter "synced_at", type: :string, label: :field_entra_id_synced_at
       end
     end
   end
